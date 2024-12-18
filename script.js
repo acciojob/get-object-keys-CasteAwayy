a7c3f44d03916fabf8d27a9d18bba1d8752971f2
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-18 18:27:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+"use strict";
+
+
+const objectPrototype  ={
+    getKeys(){
+        return Object.keys(this);
+    }
+}
+
+const student = Object.create(objectPrototype);
+
+student.name = 'abhishek';
+console.log(student.getKeys());
